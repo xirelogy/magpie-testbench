4a50f97fde4635ed9c4c2d62f96f96ed0a6db963
jxtou <jxtou@xirelogy.com>
2024-01-05 14:44:56 +0800

    Add assertTimeEquals()

--- src/Testing/TestCase.php
+++ src/Testing/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace MagpieLib\TestBench\Testing;
 
+use DateTimeInterface;
 use PHPUnit\Framework\TestCase as PhpUnitTestCase;
 
 /**
@@ -9,5 +10,15 @@ use PHPUnit\Framework\TestCase as PhpUnitTestCase;
  */
 abstract class TestCase extends PhpUnitTestCase
 {
-
+    /**
+     * Asserts that two date/time values are equal.
+     * @param DateTimeInterface|null $expected
+     * @param DateTimeInterface|null $actual
+     * @param string $message
+     * @return void
+     */
+    protected function assertTimeEquals(?DateTimeInterface $expected, ?DateTimeInterface $actual, string $message = '') : void
+    {
+        $this->assertEquals($expected?->getTimestamp(), $actual?->getTimestamp(), $message);
+    }
 }
