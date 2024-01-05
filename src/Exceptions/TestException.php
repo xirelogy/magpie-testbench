1a38ba61ac67b7717b81a892d168cdf69df058cd
jxtou <jxtou@xirelogy.com>
2024-01-05 14:45:33 +0800

    Add TestException

---
+++ src/Exceptions/TestException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MagpieLib\TestBench\Exceptions;
+
+use Magpie\Exceptions\SafetyCommonException;
+
+/**
+ * Testing related exceptions
+ */
+abstract class TestException extends SafetyCommonException
+{
+
+}
