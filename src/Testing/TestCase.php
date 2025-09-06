a0960d5554057ba295a37fa0b05a4c106168973a
jxtou <jxtou@xirelogy.com>
2025-09-06 16:15:33 +0800

    Support for getScopedItems() and runInScope()

--- src/Testing/TestCase.php
+++ src/Testing/TestCase.php
@@ -3,15 +3,88 @@
 namespace MagpieLib\TestBench\Testing;
 
 use DateTimeInterface;
+use Exception;
+use Magpie\Facades\Log;
+use Magpie\General\Contexts\Scoped;
+use Magpie\General\Contexts\ScopedCollection;
 use MagpieLib\TestBench\Http\Defaults\DefaultMockHttpClient;
 use MagpieLib\TestBench\Http\MockHttpClient;
 use PHPUnit\Framework\TestCase as PhpUnitTestCase;
+use Throwable;
 
 /**
  * A test case
  */
 abstract class TestCase extends PhpUnitTestCase
 {
+    /**
+     * @var bool If ran in scope
+     */
+    private bool $isRunInScope = false;
+
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp() : void
+    {
+        parent::setUp();
+
+        // Reset
+        $this->isRunInScope = false;
+    }
+
+
+    /**
+     * @inheritDoc
+     * @throws Exception
+     */
+    protected function tearDown() : void
+    {
+        parent::tearDown();
+
+        if (static::isForceRunInScope() && !$this->isRunInScope) {
+            throw new Exception('test should be executed in runInScope()');
+        }
+    }
+
+
+    /**
+     * All scoped items
+     * @return iterable<Scoped>
+     */
+    protected function getScopedItems() : iterable
+    {
+        return [];
+    }
+
+
+    /**
+     * Run in scope
+     * @param callable():void $fn
+     * @return void
+     * @throws Throwable
+     */
+    protected final function runInScope(callable $fn) : void
+    {
+        // Flag
+        $this->isRunInScope = true;
+
+        // Setup scope
+        $scoped = new ScopedCollection($this->getScopedItems());
+
+        try {
+            $fn();
+            $scoped->succeeded();
+        } catch (Throwable $ex) {
+            $scoped->crash($ex);
+            throw $ex;
+        } finally {
+            $scoped->release();
+        }
+    }
+
+
     /**
      * Asserts that two date/time values are equal.
      * @param DateTimeInterface|null $expected
@@ -32,5 +105,15 @@ abstract class TestCase extends PhpUnitTestCase
     protected function getHttpClient() : MockHttpClient
     {
         return DefaultMockHttpClient::create();
+    }
+
+
+    /**
+     * If runInScope() should be forced
+     * @return bool
+     */
+    protected static function isForceRunInScope() : bool
+    {
+        return false;
     }
 }
