7fc976d9fc8633aa3dabdaf698c3f5d5e7b79574
jxtou <jxtou@xirelogy.com>
2025-09-07 17:01:36 +0800

    Add CacheCleared test-env prepare: Clear all old values in cache

---
+++ src/Configurations/Prepares/CacheCleared.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace MagpieLib\TestBench\Configurations\Prepares;
+
+use Magpie\Caches\CacheProvider;
+use Magpie\General\Traits\StaticCreatable;
+use MagpieLib\TestBench\Configurations\Concepts\TestEnvironmentPreparable;
+use MagpieLib\TestBench\Configurations\TestEnvironmentContext;
+
+/**
+ * Clear everything in the corresponding cache provider
+ */
+class CacheCleared implements TestEnvironmentPreparable
+{
+    use StaticCreatable;
+
+    /**
+     * @inheritDoc
+     */
+    public function prepare(TestEnvironmentContext $context) : void
+    {
+        $logger = $context->getLogger();
+
+        $logger->info('Clearing cache...');
+        $provider = CacheProvider::getDefaultProvider();
+        $provider->clear();
+
+        $logger->info('Cache cleared');
+    }
+}
